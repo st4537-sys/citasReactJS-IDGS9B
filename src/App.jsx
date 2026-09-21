100d3b8faff6c6da0f864afb70e22b49ee9e445f
unknown <st4537@utr.edu.mx>
2026-09-21 11:17:00 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+      <p>Primer componente con React y y Vite </p>
+    </>
+  )
+}
+
+export default App
